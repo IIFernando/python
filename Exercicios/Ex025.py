9a1fa4bb2c31d9bb87a58852394b76f558ac492c
Fernando <fernandoaraujocontato@outlook.com>
2024-11-26 14:22:36 -0300

    novo exercicio

--- Exercicios/Ex025.py
+++ Exercicios/Ex025.py
@@ -9,10 +9,33 @@ possibilidade para o usuário digitar uma letra, você vai conferir se a letra d
 
 palavra = 'fernando'
 
+acerto = 0
+erro = 0
+
 while True:
 
     letra = input('Digite uma letra: ')
 
-    for l in palavra:
-        if l == letra:
-            print(f'Você acertou uma letra. Palavra secreta: {len(palavra) * '*'}')
+    if letra in palavra:
+        acerto += 1
+        print(f'Você acertou a letra {letra} faz parte da palavra secreta. Acertos N° {acerto}.')
+    elif letra not in palavra:
+        erro += 1
+        print(f'Infelizmente a letra {letra} não faz parte da palavra secreta. Erros {erro}.')
+
+    if acerto == len(palavra):
+        print(f'Parabens você acertou todas as letras com um numero total de {acerto + erro} tentativas, a palavra secreta é {palavra}')
+        break
+
+    while True:
+        resp = input('Deseja continuar? [s/n] ')
+        if resp == 's':
+            break
+        elif resp == 'n':
+            break
+        else:
+            print('Opção inválida. Tente novamente.')
+
+    if resp == 'n':
+        print('Você desistiu!!!')
+        break
